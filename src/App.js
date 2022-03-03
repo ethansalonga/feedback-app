b64381f8a0cad1a79cf4c8078a5ae527cc5f8ed8
Ethan Salonga <ethansalonga28@gmail.com>
2022-03-03 13:00:59 -0500

    create context and provider

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
-import FeedbackData from "./data/FeedbackData"
-import AboutPage from "./pages/AboutPage"
 import AboutIconLink from "./components/AboutIconLink"
+import AboutPage from "./pages/AboutPage"
+import { FeedbackProvider } from "./context/FeedbackContext"
+import FeedbackData from "./data/FeedbackData"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -24,30 +25,32 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          ></Route>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
 
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
