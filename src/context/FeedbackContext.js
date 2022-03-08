7c172b41dfebaa0c520a579ccfeb37fd862bfe5c
Ethan Salonga <ethansalonga28@gmail.com>
2022-03-07 19:41:32 -0500

    edit feedback event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,15 +22,30 @@ export const FeedbackProvider = ({ children }) => {
     },
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  });
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
+  }
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
   }
 
   return (
@@ -38,7 +53,8 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback
       }}
     >
       {children}
