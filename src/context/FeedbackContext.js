2f5f0341af161664be93a5070fcf6daf1f23ccac
Ethan Salonga <ethansalonga28@gmail.com>
2022-03-13 11:53:40 -0400

    add feedback and setting up proxy

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid"
 import { createContext, useState, useEffect } from "react"
 
 const FeedbackContext = createContext()
@@ -19,7 +18,7 @@ export const FeedbackProvider = ({ children }) => {
   // Fetch feedback
   const fetchFeedback = async () => {
     const response = await fetch(
-      `http://localhost:5000/feedback?_sort=id&_order=desc`
+      `/feedback?_sort=id&_order=desc`
     )
     const data = await response.json()
 
@@ -28,8 +27,16 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
     setFeedback([newFeedback, ...feedback])
   }
 
